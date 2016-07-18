ac5ca0fae5daaba1ff8db236c5e7cab3da7dde66
刀哥 <liufan321@gmail.com>
2016-07-18 12:30:03 +0800

    增加控制流和可选项判断

--- 基础语法/Swift基础语法.playground/Pages/003-控制流.xcplaygroundpage/Contents.swift
+++ 基础语法/Swift基础语法.playground/Pages/003-控制流.xcplaygroundpage/Contents.swift
@@ -1,12 +1,11 @@
 import Foundation
 
 /*:
- ## 内容提纲
+ ## 知识点
  
  * if
  * 三目
  * switch
- * 可选项判断
  
  ### if
  * Swift 中没有 C 语言中的**非零即真**概念
@@ -15,7 +14,7 @@ import Foundation
  * 但是 `{}` 不能省略
  */
 
-let num = 10
+let num = 100
 
 if num > 10 {
     print("大 \(num)")
@@ -23,4 +22,50 @@ if num > 10 {
     print("小 \(num)")
 }
 
+/*:
+ ### 三目
+ 
+ * Swift 中的 三目 运算保持了和 OC 一致的风格
+ * 提示：以下代码，可以按 `cmd + shift + y`，打开控制台，查看输出结果
+ */
+num > 10 ? print("大 \(num)") : print("小 \(num)")
+
+/*:
+ ### switch
+ * `switch` 不再局限于整数
+ * `switch` 可以针对任意数据类型进行判断
+ * 每一个 `case` 后面必须有可以执行的语句
+ * 不再强制需要 `break`
+ * 如果没有任何需要执行的语句，可以使用 `break`
+ * 要保证处理所有可能的情况，不然编译器直接报错，不处理的条件可以放在 `default` 分支中
+ * 每一个 `case` 中定义的变量仅在当前 `case` 中有效，而 OC 中需要使用 {}
+ * 多值 case 可以使用 `,` 分隔
+ */
+
+let scoreString = "优"
+
+switch scoreString {
+case "优":
+    let name = "学生"
+    print(name + "80~100分")
+case "良", "中": print("60~80分")
+case "差": print("不及格")
+default: break
+}
+
+/*:
+ #### switch 的条件判断
+ * switch 中可以使用 `where` 子句判断条件
+ * 如果只做条件判断，`case` 部分可以使用 `_` 省略
+ * 提示：Swift 中可以使用 `_` 忽略任何不关心的内容
+ */
+let score = 90
+switch score {
+case _ where score >= 80: print("优")
+case _ where score >= 70: print("良")
+case _ where score >= 60: print("中")
+default: print("差")
+}
+
+//: 有关本示例的许可信息，请参阅：[许可协议](License)
 
---
+++ 基础语法/Swift基础语法.playground/Pages/004-可选项判断.xcplaygroundpage/Contents.swift
@@ -0,0 +1,67 @@
+import Foundation
+
+/*:
+ 由于可选项的值可能为 `nil`，不允许直接参与运算，因此在实际开发中，经常需要判断可选项是否有值。
+ 
+ 如果单纯使用 if，会让代码嵌套层次很深，不宜阅读和维护，为了解决这一问题，苹果提供了以下三种方式：
+ 
+ * `??`
+ * `if let / var`
+ * `guard let / var`
+ 
+ ## ??
+ 
+ * `??` 运算符可以用于判断可选项是否为 `nil`，如果是则使用后面的值替代
+ 
+ > 注意：`??` 的优先级低，在使用时，应该注意使用 `()`
+ */
+
+let x: Int? = 10
+let y: Int? = 100
+print((x ?? 0) + (y ?? 0))
+
+/*:
+ ## if `let / var`
+ 
+ * 使用 `if let`，一旦进入 `if` 分支，表示可选项一定有值
+ * `常量/变量`的作用域仅在 `{}` 内部
+ * 使用 `,` 可以判断多个可选项是否有值
+ * 使用同名常量/变量，避免再次起名的烦恼
+ * 如果要在分支逻辑中修改值，可以使用 `var`，通常使用 `let`
+ */
+
+let name: String? = "Mike"
+let age: Int? = 18
+
+if let name = name, age = age {
+    print("\(name) 今年 \(age) 岁")
+} else {
+    print("姓名或者年龄为 nil")
+}
+
+/*:
+ ## guard 
+ 
+ * `guard` 是与 `if let` 相反的语法，Swift 2.0 推出
+ * `guard` 同样可以判断可选项是否有值
+ * 多值判断使用 `,` 分隔
+ * 如果发现 `nil`，在 `else` 分支返回
+ * 分支结束后，所有的 `常量/变量` 都有值
+ * 相比较 `if let`，`guard` 语法的分支结构能节省一层
+ * 如果要在后续逻辑中修改值，可以使用 `var`，通常使用 `let`
+ */
+
+func demo(name: String?, age: Int?) {
+
+    guard let name = name, age = age else {
+        print("姓名或者年龄为 nil")
+        
+        return
+    }
+    
+    print("\(name) 今年 \(age) 岁")
+}
+
+demo(name: name, age: age)
+
+//: 有关本示例的许可信息，请参阅：[许可协议](License)
